76e5e30969a6927ae7b1d3210cc50748b11f215a
Ali Toyn <ali.toyn@gmail.com>
2020-06-11 21:01:14 +0100

    create a delete command that will only delete folders called by names of the month but are not on the keep list

--- backup_auto_delete.py
+++ backup_auto_delete.py
@@ -1,4 +1,4 @@
-import datetime
+import datetime, os
 
 # import data from config
 from config import *
@@ -18,12 +18,23 @@ month_num = int(date.month) - 1
 # convert to words
 # print(months[month_num])
 
-# get months to delete
-for i in range(month_num - backlog, month_num):
+# get months to keep
+to_keep = ''
+for i in range(month_num + 1 - backlog, month_num + 1):
 	if i < 0:
 		i += 12
+	to_keep += months[i]
 
-	print(months[i])
+	if i != month_num:
+		to_keep += '|'
+
+
+# This command passes the directory through the possibile months so that additional folders are not deleted
+# this is then passed through the list of folders to keep
+# any folders that make it to the rm command are ones to delete
+cmd = 'ls | grep -E "January|February|March|April|May|June|July|August|September|October|November|December" | grep -vE "'+ to_keep +'" | xargs rm -r' 
+
+print(cmd)
 
 
 # delete old data
